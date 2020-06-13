1bb97eb0cd2821f96b557f340763bed0374adf73
Marc Kuiper <marckuiper@gmail.com>
2020-06-13 16:41:45 +0200

    Renamed fragment view

--- app/src/main/java/com/boardgamegeek/ui/CalendarFragment.kt
+++ app/src/main/java/com/boardgamegeek/ui/CalendarFragment.kt
@@ -18,7 +18,7 @@ class CalendarFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View {
-        return inflater.inflate(R.layout.calendar_fragment, container, false)
+        return inflater.inflate(R.layout.fragment_calendar, container, false)
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
