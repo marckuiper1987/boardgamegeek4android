76a9e348ec42b5937b602e8ea2a465df7d37f16f
Marc Kuiper <marckuiper@gmail.com>
2020-07-31 23:32:58 +0200

    Use rounded rects and improved calendar thumbnail performance and contrast.

--- app/src/main/java/com/boardgamegeek/ui/HistoryCalendarThumbnail.kt
+++ app/src/main/java/com/boardgamegeek/ui/HistoryCalendarThumbnail.kt
@@ -4,26 +4,25 @@ import android.content.Context
 import android.graphics.Canvas
 import android.graphics.Paint
 import android.graphics.Paint.ANTI_ALIAS_FLAG
+import android.graphics.RectF
 import android.util.AttributeSet
 import android.view.View
 import com.boardgamegeek.R
 import com.boardgamegeek.entities.PlayEntity
-import kotlinx.android.synthetic.main.row_collection.view.year
 import java.time.LocalDate
 import java.time.YearMonth
 
+
 private const val weekDays = 7
 private const val numWeeks = 6
-private const val margin = 1
+
+private const val padding = 8
+private const val rectMargin = 1
+private const val rectRadius = 5f
 
 class HistoryCalendarThumbnail(context: Context, attrs: AttributeSet) : View(context, attrs) {
 
-    private val clearPaint = Paint(ANTI_ALIAS_FLAG).apply {
-        color = context.resources.getColor(R.color.light_blue)
-        style = Paint.Style.FILL
-    }
-
-    private val filledPaint = Paint(ANTI_ALIAS_FLAG).apply {
+    private val paint = Paint(ANTI_ALIAS_FLAG).apply {
         color = context.resources.getColor(R.color.background)
         style = Paint.Style.FILL
     }
@@ -37,6 +36,7 @@ class HistoryCalendarThumbnail(context: Context, attrs: AttributeSet) : View(con
     var playsByDate: Map<LocalDate, List<PlayEntity>>? = null
         set(value) {
             field = value
+            setup()
             requestLayout()
         }
 
@@ -48,45 +48,70 @@ class HistoryCalendarThumbnail(context: Context, attrs: AttributeSet) : View(con
         setMeasuredDimension(w, h)
     }
 
+    private val rects = mutableListOf<Pair<RectF,DayProps>>()
+
+    data class DayProps(
+        val inOtherMonth: Boolean,
+        val playNum: Int
+    )
+
+    private fun setup() {
+
+        rects.clear()
+
+        val yearMonth = yearMonth ?: return
+
+        val w = width.toFloat() - (padding * 2)
+        val cellW = (w - (rectMargin * (weekDays - 1))) / weekDays
+
+        val firstDayOfWeek = yearMonth.atDay(1).dayOfWeek.value
+
+        for (week in 1 until numWeeks + 1) {
+            for (dayOfWeek in 1 until weekDays + 1) {
+
+                val day = (((week - 1) * weekDays) + dayOfWeek - firstDayOfWeek) + 1
+                val dayInOtherMonth = day <= 0 || day > yearMonth.lengthOfMonth()
+
+                val plays =
+                    if (!dayInOtherMonth)
+                        playsByDate?.get(yearMonth.atDay(day)) ?: emptyList()
+                    else emptyList()
+
+                val props = DayProps(
+                    inOtherMonth = dayInOtherMonth,
+                    playNum = plays.size
+                )
+
+                val left = padding + (dayOfWeek - 1) * (cellW + rectMargin)
+                val right = left + cellW
+                val top = padding + (week - 1) * (cellW + rectMargin)
+                val bottom = top + cellW
+
+                rects.add(RectF(left, top, right, bottom) to props)
+            }
+        }
+    }
+
     override fun onDraw(canvas: Canvas?) {
         canvas?.apply {
-
-            val yearMonth = yearMonth ?: return
-
-            val w = width.toFloat()
-            val cellW = (w - (margin * (weekDays - 1))) / weekDays
-
-            val firstDayOfWeek = yearMonth.atDay(1).dayOfWeek.value
-
-            for (week in 1 until numWeeks + 1) {
-                for (dayOfWeek in 1 until weekDays + 1) {
-
-                    val day = (((week - 1) * weekDays) + dayOfWeek - firstDayOfWeek) + 1
-                    val dayInOtherMonth = day <= 0 || day > yearMonth.lengthOfMonth()
-
-                    val plays =
-                        if (!dayInOtherMonth)
-                            playsByDate?.get(yearMonth.atDay(day)) ?: emptyList()
-                        else emptyList()
-
-                    val paint = when {
-                        dayInOtherMonth ->
-                            filledPaint.apply { alpha = 40 }
-                        plays.isNotEmpty() ->
-                            filledPaint.apply {
-                                alpha = (155 + (plays.size * 20)).coerceAtMost(255)
-                            }
-                        else ->
-                            filledPaint.apply { alpha = 100 }
+            rects.forEach {
+                drawRoundRect(
+                    it.first,
+                    rectRadius,
+                    rectRadius,
+                    it.second.let { (inOtherMonth, playNum) ->
+                        when {
+                            inOtherMonth ->
+                                paint.apply { alpha = 20 }
+                            playNum != 0 ->
+                                paint.apply {
+                                    alpha = (155 + (playNum * 20)).coerceAtMost(255)
+                                }
+                            else ->
+                                paint.apply { alpha = 60 }
+                        }
                     }
-
-                    val left = (dayOfWeek - 1) * (cellW + margin)
-                    val right = left + cellW
-                    val top = (week - 1) * (cellW + margin)
-                    val bottom = top + cellW
-
-                    drawRect(left, top, right, bottom, paint)
-                }
+                )
             }
         }
     }
